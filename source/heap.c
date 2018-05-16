6aec4548d28961c2060416f4331f418d4c782745
Evan Dougal <edougal@gmail.com>
2018-05-15 19:43:31 -0700

    heap setup

--- source/heap.c
+++ source/heap.c
@@ -1,4 +1,27 @@
+
 #include <stdio.h> 	// printf
 #include <stddef.h> // NULL 
 
 
+// option to use explicit or implicit datastructure
+#ifndef HEAP_USE_IMPLICIT_DATASTRUCTURE
+#define HEAP_USE_IMPLICIT_DATASTRUCTURE 0
+#endif
+
+#if HEAP_USE_IMPLICIT_DATASTRUCTURE == 1
+//implicit implementation
+
+
+
+
+
+#elif 
+// explicit implementation
+
+
+
+
+
+
+
+#endif 
---
+++ source/heap.h
@@ -0,0 +1,6 @@
+
+#ifndef __HEAP__H
+#define __HEAP__H
+
+
+#endif
